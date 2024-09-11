4b4b2e088404221bd04fd2d370869af93b8dcf51
banagaurav <gauravbana840@gmail.com>
2024-09-11 14:32:39 +0545

    Add Models/Category.cs

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
