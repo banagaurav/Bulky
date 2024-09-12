6ae1c5b6e2a1ceca7b9c4683bc7a34b700986eb3
banagaurav <gauravbana840@gmail.com>
2024-09-12 15:13:47 +0545

    add create icon in view to new Category, add action method Create() in CategoryController.cs

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,6 +16,9 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
-
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
