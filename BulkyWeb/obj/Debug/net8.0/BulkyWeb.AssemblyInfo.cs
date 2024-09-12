f79b375678b01422b4761809a09499bf2ce28dd7
banagaurav <gauravbana840@gmail.com>
2024-09-12 15:17:55 +0545

    add

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a827bdafd77a8ecb7c5b40a2572bf32a50a874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ae1c5b6e2a1ceca7b9c4683bc7a34b700986eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
