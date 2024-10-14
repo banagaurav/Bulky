45c35fba2c62a9f54d423ddf444a10b179ef3a94
banagaurav <gauravbana840@gmail.com>
2024-10-14 12:48:35 +0545

    edited views

--- Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
+++ Bulky.Models/obj/Debug/net8.0/Bulky.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736591d4ac59b5ba9a8aaa68e0c8db141680de8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d43b7486fc13cc127fc5df948a523d74cc2d471")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736591d4ac59b5ba9a8aaa68e0c8db141680de8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d43b7486fc13cc127fc5df948a523d74cc2d471")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
