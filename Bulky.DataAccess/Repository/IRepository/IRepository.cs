cd44d14d34dd1502ff36d3eb80beea7b41f162a1
banagaurav <gauravbana840@gmail.com>
2024-10-24 20:47:13 +0545

    Repository interface implemented

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -3,13 +3,12 @@ using System.Linq.Expressions;
 
 namespace Bulky.DataAccess.Repository.IRepository;
 
-internal interface IRepository<T> where T : class
+public interface IRepository<T> where T : class
 {
     //T - Category
     IEnumerable<T> GetAll();
     T Get(Expression<Func<T, bool>> filter);
     void Add(T entity);
-    void Update(T entity);
     void Remove(T entity);
     void RemoveRange(IEnumerable<T> entity);
 }
